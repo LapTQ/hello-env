f6cf420c35b2672d552dc736dbfc08d9b70a01d5
LapTQ <lap.tq4@gmail.com>
2024-08-11 23:37:05 +0700

    delete redundance

--- templates/vnc/compose-up.sh
+++
@@ -1,7 +0,0 @@
-export USER_ID=$(id -u)
-export USER_NAME=$(id -un)
-export GROUP_ID=$(id -g)
-export GROUP_NAME=$(id -gn)
-# docker build . -t base-laptq-img -f Dockerfile-base
-# docker-compose build
-docker-compose up -d
--- templates/vnc/scripts/start-vncserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f /tmp/.X1-lock
-rm -f /tmp/.X11-unix/X1
-echo "starting VNC server ..."
-vncserver :1 -geometry 1920x1080 -depth 24 && tail -F /root/.vnc/*.log
